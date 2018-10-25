174c54855184377c1161a6aa78610f97c8ce9d0b
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-24 22:23:31 -0700

    Preps deployment.

--- server.js
+++ server.js
@@ -21,6 +21,25 @@ const db = knex({
   },
 });
 
+// Heroku Database Setup
+const {Client} = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+client.query(
+  'SELECT table_schema,table_name FROM information_schema.tables;',
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  },
+);
+
 // Launch
 const express = require('express');
 const app = express();
@@ -31,14 +50,8 @@ app.use(bodyParser.json());
 // Allow cross origin requests.
 app.use(cors());
 
-// Homepage
-app.get('/', (req, res) => {
-  db.select('*')
-    .from('users')
-    .then(users => res.send(users));
-});
-
 // Routes
+app.get('/', (req, res) => res.send('Server working.'));
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfile(db));
@@ -46,4 +59,5 @@ app.put('/image', image.handleImage(db));
 app.post('/imageurl', image.handleApiCall);
 
 // Start server
-app.listen(3000);
+let port = parseInt(process.env.PORT) || 8000;
+app.listen(port);
