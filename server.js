101170f573e49ebced5afbe4f25e7ba378c7c98a
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-25 00:15:22 -0700

    Adds environment databases.

--- server.js
+++ server.js
@@ -11,17 +11,18 @@ const signin = require("./controllers/signin");
 
 // Setup Database
 const knex = require("knex");
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'kevin',
-//     password: '',
-//     database: 'smart-brain',
-//   },
-// });
 
-const db = knex({
+const local_db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "kevin",
+    password: "",
+    database: "smart-brain"
+  }
+});
+
+const prod_db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
@@ -29,24 +30,7 @@ const db = knex({
   }
 });
 
-// // Heroku Database Setup
-// const {Client} = require('pg');
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
-//
-// client.connect();
-// client.query(
-//   'SELECT table_schema,table_name FROM information_schema.tables;',
-//   (err, res) => {
-//     if (err) throw err;
-//     for (let row of res.rows) {
-//       console.log(JSON.stringify(row));
-//     }
-//     client.end();
-//   },
-// );
+const db = process.env.USE_LOCAL_DATABASE ? local_db : prod_db;
 
 // Launch
 const express = require("express");
